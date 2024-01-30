5c159635c5c5ee2f3e17880153c3d84957f6c100
DaLongZZi <1335941759@qq.com>
2024-01-30 19:29:57 +0800

    分辨率

--- DLZZ.sh
+++ DLZZ.sh
@@ -9,9 +9,10 @@ main_menu() {
         --title "IOAOSP v0.1" \
         --menu "请选择功能:" 15 40 4 \
         1 "安装软件" \
-        2 "更新本工具" \
-        3 "初始化工具（仅需执行一次）" \
-        4 "退出" \
+        2 "系统功能" \
+        3 "更新本工具" \
+        4 "初始化工具（仅需执行一次）" \
+        5 "退出" \
     3>&1 1>&2 2>&3)
     
     case $CHOICE in
@@ -21,13 +22,17 @@ main_menu() {
         ;;
         2)
             echo "正在删除本地文件并更新"
-            execute_option_2
+            execute_option_4
         ;;
         3)
+            echo "正在删除本地文件并更新"
+            execute_option_2
+        ;;
+        4)
             echo "初始化"
             execute_option_3
         ;;
-        4)
+        5)
             echo "退出"
             exit 0
         ;;
@@ -49,11 +54,11 @@ execute_option_1() {
     case $CHOICE in
         1)
             echo "升级所有软件包"
-            execute_o_1
+            execute_1_1
         ;;
         2)
             echo "安装WPS Office"
-            execute_o_2
+            execute_1_2
         ;;
         3)
             echo "返回主界面"
@@ -64,14 +69,15 @@ execute_option_1() {
     esac
 }
 
-execute_o_1() {
+execute_1_1() {
+    # 在这里添加子操作1-1的代码
     apt update
     apt upgrade -y
     # 弹出确认窗口
     dialog --clear --backtitle "DaLongZhuaZi" --title "更新完成" --msgbox "更新软件包完成，请按回车返回主界面" 10 30
 }
 
-execute_o_2() {
+execute_1_2() {
     # 在这里添加子操作1-2的代码
     apt update
     apt install wps-office wps-office-fonts -y
@@ -99,9 +105,59 @@ DLZZ() {\
 dlzz() {\
         sh /root/IOAOSP/DLZZ.sh\
     }' /etc/zsh/zshrc
-    
+    sed -i '17i\
+# 定义 restartvnc 命令\
+restartvnc() {
+        stopvnc
+        startvnc
+    }' /etc/zsh/zshrc
     # 弹出确认窗口
     dialog --clear --backtitle "DaLongZhuaZi" --title "初始化完成" --msgbox "请在退出工具后手动输入source /etc/zsh/zshrc并回车" 10 30
+}
+
+execute_option_4() {
+    # 在这里添加操作4的代码
+    CHOICE=$(dialog --clear --backtitle "DaLongZhuaZi" \
+        --title "系统功能" \
+        --menu "请选择:" 15 40 4 \
+        1 "修改桌面分辨率" \
+        2 "清理安装过程中的错误" \
+        3 "返回主界面" \
+    3>&1 1>&2 2>&3)
+    
+    case $CHOICE in
+        1)
+            echo "修改桌面分辨率"
+            execute_4_1
+        ;;
+        2)
+            echo "清理安装过程中的错误"
+            execute_4_2
+        ;;
+        3)
+            echo "返回主界面"
+        ;;
+        *)
+            echo "未知选项"
+        ;;
+    esac
+}
+
+execute_4_1() {
+    # 使用 dialog 创建输入框，确保输入有效的分辨率格式
+    resolution=$(dialog --inputbox "请输入新的分辨率（例如：2880x1440）:" 10 30 --stdout --no-cancel)
+
+    # 检查用户输入是否符合有效的分辨率格式
+    if [[ $resolution =~ ^[0-9]+x[0-9]+$ ]]; then
+        # 在文件中替换第 20 行的内容为用户输入的分辨率
+        sed -i "20s/.*/geometry=$resolution/" /etc/tigervnc/vncserver-config-tmoe
+
+        # 弹出确认窗口
+        dialog --clear --backtitle "DaLongZhuaZi" --title "设置完成" --msgbox "分辨率设置完成，请输入restartvnc来重启vnc服务，按回车返回主界面" 10 30
+    else
+        # 如果用户输入不符合格式，弹出错误提示窗口
+        dialog --clear --backtitle "DaLongZhuaZi" --title "错误" --msgbox "无效的分辨率格式，请重新输入。" 10 30
+    fi
 }
 
 # 运行主选择界面
