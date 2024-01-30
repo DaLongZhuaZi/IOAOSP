c12aca2ffba5fef9c5ede6415edaf41ba922e35b
DaLongZZi <1335941759@qq.com>
2024-01-30 20:54:00 +0800

    fix

--- DLZZ.sh
+++ DLZZ.sh
@@ -109,7 +109,6 @@ execute_option_2() {
     chmod +x /root/IOAOSP/DLZZ.sh
     chmod +x /root/IOAOSP
     
-    
     # 获取当前脚本的绝对路径
     script_path=$(realpath "$0")
     
@@ -127,26 +126,25 @@ execute_option_2() {
     exec "$script_path" "$@"
 }
 
-# 其他脚本逻辑...
-
 execute_option_3() {
     # 在这里添加操作3的代码
-    sed -i '15i\
+    cat <<EOL >> /etc/zsh/zshrc
+
 # 定义 DLZZ 命令\
 DLZZ() {\
         sh /root/IOAOSP/DLZZ.sh\
     }' /etc/zsh/zshrc
-    sed -i '16i\
 # 定义 dlzz 命令\
 dlzz() {\
         sh /root/IOAOSP/DLZZ.sh\
     }' /etc/zsh/zshrc
-    sed -i '17i\
 # 定义 restartvnc 命令\
 restartvnc() {
         stopvnc
         startvnc
     }' /etc/zsh/zshrc
+
+EOL
     # 弹出确认窗口
     dialog --clear --backtitle "DaLongZhuaZi" --title "初始化完成" --msgbox "请在退出工具后手动输入source /etc/zsh/zshrc并回车" 10 30
 }
